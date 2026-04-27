ffd678105a61e5b98ab673979af6982200aaf004
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-27 20:50:00 +0200

    Added saveusertodatabase to userrepimpl

--- HabitTracker/User/Repositories/UserRepositoryImpl.swift
+++ HabitTracker/User/Repositories/UserRepositoryImpl.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+
+class UserRepositoryImpl: UserRepository {
+    
+    
+    private var db = Firestore.firestore()
+    func saveUserToDatabase(username: String, email: String) async throws {
+        
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        let newUser = User(id: uid, username: username, email: email, joined: Date().timeIntervalSince1970)
+        
+        try await db.collection("users")
+            .document(uid)
+            .setData(from: newUser)
+    }
+    
+    
+
+
+   
+    
+    
+
+    
+    
+    
+   
+}
