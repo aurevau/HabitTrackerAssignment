1b073d38fa9078dd7faf50fb574e791d92cc8286
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-27 11:13:20 +0200

    Created AuthReposiotry which implements protocol functions

--- HabitTracker/Auth/Repositories/AuthRepositoryImpl.swift
+++ HabitTracker/Auth/Repositories/AuthRepositoryImpl.swift
@@ -5,4 +5,32 @@
 //  Created by Aurelie Vaudan APP25 on 2026-04-27.
 //
 
-import Foundation
+import Observation
+import FirebaseAuth
+
+@Observable
+class AuthRepositoryImpl: AuthRepository {
+    func register(email: String, password: String) async throws -> FirebaseAuth.AuthDataResult {
+        try await Auth.auth().createUser(withEmail: email, password: password)
+    }
+    
+    func login(email: String, password: String) async throws {
+        try await Auth.auth().signIn(withEmail: email, password: password)
+    }
+    
+    func logOut() throws {
+        try Auth.auth().signOut()
+    }
+    
+    func isUserSignedIn() -> Bool {
+        return Auth.auth().currentUser != nil
+    }
+    
+    func getUserId() -> String? {
+        return Auth.auth().currentUser?.uid
+    }
+    
+    
+}
+
+
