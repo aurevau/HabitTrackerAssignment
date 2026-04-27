ae2130e0465c8d5a54a7d7e478f4e730c9dee975
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-27 10:56:30 +0200

    Configured gitignore for project

---
+++ HabitTracker/ViewModifiers/GradientCard.swift
@@ -0,0 +1,8 @@
+//
+//  GradientCard.swift
+//  HabitTracker
+//
+//  Created by Aurelie Vaudan APP25 on 2026-04-27.
+//
+
+import Foundation
---
+++ HabitTracker/Views/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HabitTracker
+//
+//  Created by Aurelie Vaudan APP25 on 2026-04-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+          
+            
+            
+           
+        }
+        .gradientBackground()
+    }
+}
+
+#Preview {
+    ContentView()
+}
