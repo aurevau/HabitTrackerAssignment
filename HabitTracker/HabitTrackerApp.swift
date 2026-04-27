9de782432f39d00c4d5979920cc507d0f1e72659
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-27 09:25:04 +0200

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Aurelie Vaudan APP25 on 2026-04-27.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
