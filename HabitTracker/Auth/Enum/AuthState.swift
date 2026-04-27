d28e1e5f6c26275280f63a098a84603fb8e0c450
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-27 11:04:17 +0200

    Added authstate to be able to check user and navigate from that

---
+++ HabitTracker/Auth/Enum/AuthState.swift
@@ -0,0 +1,15 @@
+//
+//  AuthState.swift
+//  HabitTracker
+//
+//  Created by Aurelie Vaudan APP25 on 2026-04-27.
+//
+
+import Foundation
+
+enum AuthState {
+    case loggedIn
+    case loggedOut
+    case guest
+    case loading
+}
---
+++ HabitTracker/Auth/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  HabitTracker
+//
+//  Created by Aurelie Vaudan APP25 on 2026-04-27.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
