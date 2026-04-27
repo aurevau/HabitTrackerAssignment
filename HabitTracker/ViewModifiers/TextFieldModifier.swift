9317f54bda545cb7c841f7da8b9b1f8664fb9e74
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-27 14:16:03 +0200

    Added a textfieldmodifier

--- HabitTracker/ViewModifiers/TextFieldModifier.swift
+++ HabitTracker/ViewModifiers/TextFieldModifier.swift
@@ -5,4 +5,18 @@
 //  Created by Aurelie Vaudan APP25 on 2026-04-27.
 //
 
-import Foundation
+import SwiftUI
+
+
+struct TextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(.horizontal, 12)
+            .padding(.vertical, 10)
+            .frame(width: 352, height: 50)
+            .font(.subheadline)
+            .background(Color(.systemGray6))
+            .cornerRadius(10)
+            
+    }
+}
