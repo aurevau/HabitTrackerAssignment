617bedf854e89649481a274cdcc682f0451d2430
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-27 14:14:21 +0200

    Initial design and logics for loggin in implemented on loginview

--- HabitTracker/Auth/Views/LoginView.swift
+++ HabitTracker/Auth/Views/LoginView.swift
@@ -8,11 +8,71 @@
 import SwiftUI
 
 struct LoginView: View {
+    @Environment(AuthViewModel.self) private var authViewModel
+    
+    @State var showRegisterSheet: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        @Bindable var authVm = authViewModel
+        ZStack {
+            VStack(spacing: 20) {
+                Spacer()
+                TextField("Email", text: $authVm.email)
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled()
+                    .modifier(TextFieldModifier())
+                
+                SecureField("Lösenord", text: $authVm.password)
+                    .autocorrectionDisabled()
+                    .autocapitalization(.none)
+                    .modifier(TextFieldModifier())
+                
+                if !authViewModel.errorMessage.isEmpty {
+                    Text(authViewModel.errorMessage)
+                        .font(.caption)
+                        .foregroundColor(Color.theme.buttonBackground)
+                }
+                
+                Button {
+                    authViewModel.login()
+                } label: {
+                    if authViewModel.isLoading {
+                        ProgressView()
+                    } else {
+                        Text("Logga in")
+                            .modifier(ButtonModifier())
+                    }
+                }
+                
+                Button {
+                    showRegisterSheet = true
+                        
+                } label: {
+                    Text("Registrera dig")
+                        
+                }
+                .modifier(ButtonModifierReversedColors())
+                
+                Spacer()
+                Button {
+                    authViewModel.continueAsGuest()
+                } label: {
+                    Text("Fortsätt som gäst")
+                        .modifier(OutlineButtonModifier())
+                    
+                }
+                
+            }
+    
+        }
+        .sheet(isPresented: $showRegisterSheet) {
+            RegisterView()
+        }
+        .gradientBackground()
     }
 }
 
 #Preview {
     LoginView()
+        .environment(AuthViewModel())
 }
