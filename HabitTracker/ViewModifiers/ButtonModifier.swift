305a5ccb759b09cf14ce260d6db3a485743f99d3
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-27 14:16:15 +0200

    Created a buttonmodifier

--- HabitTracker/ViewModifiers/ButtonModifier.swift
+++ HabitTracker/ViewModifiers/ButtonModifier.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct ButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .fontWeight(.semibold)
+            .foregroundColor(.primaryText)
+            .frame(width: 352, height: 50)
+            .background(LinearGradient(colors: [Color.theme.cardGradientStart, Color.theme.cardGradientMid, Color.theme.cardGradientEnd], startPoint: .leading, endPoint: .trailing))
+            .cornerRadius(12)
+    }
+}
+
+
+struct ButtonModifierReversedColors: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .fontWeight(.semibold)
+            .foregroundColor(.primaryText)
+            .frame(width: 352, height: 50)
+            .background(LinearGradient(colors: [Color.theme.cardGradientEnd, Color.theme.cardGradientMid, Color.theme.cardGradientStart], startPoint: .leading, endPoint: .trailing))
+            .cornerRadius(12)
+    }
+}
+
+struct OutlineButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.subheadline)
+            .fontWeight(.semibold)
+            .foregroundColor(Color.theme.primaryText)
+            .frame(width: 352, height: 50)
+            .background(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(
+                        LinearGradient(
+                            colors: [
+                                Color.theme.cardGradientStart,
+                                Color.theme.cardGradientEnd
+                            ],
+                            startPoint: .leading,
+                            endPoint: .trailing
+                        ),
+                        lineWidth: 2
+                    )
+            )
+    }
+}
