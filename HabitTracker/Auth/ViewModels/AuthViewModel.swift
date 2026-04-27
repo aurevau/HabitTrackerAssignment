52d74f1b807e88b63aae68333a57eec0f96a3352
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-27 14:13:58 +0200

    Added register function and validations to authviewmodel

--- HabitTracker/Auth/ViewModels/AuthViewModel.swift
+++ HabitTracker/Auth/ViewModels/AuthViewModel.swift
@@ -11,12 +11,15 @@ import Foundation
 @Observable
 class AuthViewModel {
     var authState: AuthState = .guest
+    var username = ""
     var email = ""
+    var confirmEmail = ""
     var password = ""
     var confirmPassword = ""
     var errorMessage = ""
     
     var isLoading: Bool = false
+    var registerSuccess: Bool = false
     
     private let authRepo = AuthRepositoryImpl()
     
@@ -24,11 +27,11 @@ class AuthViewModel {
         authState = authRepo.isUserSignedIn() ? .loggedIn : .loggedOut
     }
     
-    func login() async {
+    func login()  {
         guard validateLogin() else {
             return
         }
-        
+        Task {
             isLoading = true
             do {
                 try await authRepo.login(email: email, password: password)
@@ -39,6 +42,77 @@ class AuthViewModel {
                 errorMessage = error.localizedDescription
                 authState = .loggedOut
             }
+            isLoading = false 
+        }
+       
+           
+    }
+    
+    func register() {
+        guard validateRegistration() else {
+            return
+        }
+        
+        Task {
+            errorMessage = ""
+            isLoading = true
+            do {
+                let result = try await authRepo.register(email: email, password: password)
+                
+                authState = .loggedIn
+                registerSuccess = true
+                
+                // Save user to firebase
+            } catch {
+                isLoading = false
+                errorMessage = error.localizedDescription
+                registerSuccess = false
+            }
+            
+            isLoading = false
+        }
+    }
+    
+    func validateRegistration() -> Bool {
+        guard !email.trimmingCharacters(in: .whitespaces).isEmpty, !confirmEmail.trimmingCharacters(in: .whitespaces).isEmpty,
+              !password.trimmingCharacters(in: .whitespaces).isEmpty, !confirmPassword.trimmingCharacters(in: .whitespaces).isEmpty, !username.trimmingCharacters(in: .whitespaces).isEmpty else {
+            errorMessage = "Vänligen fyll i alla fält!"
+            return false
+        }
+        
+        guard password == confirmPassword else {
+            errorMessage = "Lösenordet matchar inte!"
+            return false
+        }
+        
+        guard email == confirmEmail else {
+            errorMessage = "Email matchar inte"
+            return false
+        }
+        
+        guard password.count >= 6 else {
+            errorMessage = "Vänligen välj ett lösenord med minst 6 tecken."
+            return false
+        }
+        
+        guard email.contains("@") && email.contains(".") else {
+            errorMessage = "Vänligen ange en giltig epostadress."
+            return false
+        }
+        return true
+    }
+    
+    func logOut() {
+        Task {
+            do {
+                try authRepo.logOut()
+                authState = .loggedOut
+            } catch {
+                errorMessage = error.localizedDescription
+            }
+            
+        }
+
     }
     
     func continueAsGuest() {
@@ -51,7 +125,7 @@ class AuthViewModel {
         
         guard !email.trimmingCharacters(in: .whitespaces).isEmpty,
         !password.trimmingCharacters(in: .whitespaces).isEmpty else {
-            errorMessage = "Please fill in all fields"
+            errorMessage = "Vänligen fyll i alla fält!"
             return false
         }
         
