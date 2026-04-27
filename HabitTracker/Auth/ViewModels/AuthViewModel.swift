74a9a239a46ac72128c40bd34c4eb27ffc85e0eb
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-27 11:37:47 +0200

    Initial setup of authviewmodel

--- HabitTracker/Auth/ViewModels/AuthViewModel.swift
+++ HabitTracker/Auth/ViewModels/AuthViewModel.swift
@@ -5,4 +5,23 @@
 //  Created by Aurelie Vaudan APP25 on 2026-04-27.
 //
 
-import Foundation
+import Observation
+
+@Observable
+class AuthViewModel {
+    var authState: AuthState = .guest
+    var email = ""
+    var password = ""
+    var confirmPassword = ""
+    var errorMessage = ""
+    
+    var isLoading: Bool = false
+    
+    private let authRepo = AuthRepositoryImpl()
+    
+    init() {
+        authState = authRepo.isUserSignedIn() ? .loggedIn : .guest
+    }
+    
+    
+}
