038fd8fb38089da05e11370eb74f737f1181213d
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-28 12:12:32 +0200

    initial shell for addhabitview

--- HabitTracker/Habit/Views/AddHabitView.swift
+++ HabitTracker/Habit/Views/AddHabitView.swift
@@ -9,10 +9,37 @@ import SwiftUI
 
 struct AddHabitView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                VStack {
+                    HStack {
+                        Image("vana")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(maxWidth: 200)
+                        Spacer()
+                    }
+                    .padding()
+                    Spacer()
+                }
+            }
+            .gradientBackground()
+        }
+        .toolbar {
+            ToolbarItem(placement: .title) {
+                Text("Lägg till vana")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(.primaryText)
+            }
+        }
+      
     }
 }
 
 #Preview {
-    AddHabitView()
+    NavigationStack {
+        AddHabitView()
+    }
+
 }
