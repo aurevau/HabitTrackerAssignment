3833e43126a978e20903951272266b1073b3b6f6
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-29 14:52:45 +0200

    created weekview with completionpercentage

--- HabitTracker/Home/Views/WeekView.swift
+++ HabitTracker/Home/Views/WeekView.swift
@@ -7,28 +7,46 @@
 
 import SwiftUI
 
-struct LocalWeekView: View {
-    let habits: [HabitLocal]
+struct WeekView: View {
+    let habits: [Habit]
     
     var body: some View {
-        ScrollView(.horizontal, showsIndicators: false) {
-            HStack(spacing: 12) {
-                ForEach(Date().getCurrentWEek(), id: \.self) {date in
-                    
-                    WeekDayView(date: date, isToday: date.isToday, isCompleted: isDateCompleted(date))
+        ScrollViewReader {proxy in
+            
+            
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 12) {
+                    ForEach(Date().getCurrentWEek(), id: \.self) {date in
+                        
+                        WeekDayView(date: date, isToday: date.isToday, completionPercentage: completionPercentage(for: date))
+                            .id(date)
+                    }
+                }
+                .padding(16)
+                
+            }
+            .onAppear {
+                if let today = Date().getCurrentWEek().first(where: {$0.isToday }) {
+                    withAnimation {
+                        proxy.scrollTo(today, anchor: .center)
+                    }
                 }
             }
-            .padding(.horizontal, 20)
-            .padding(.vertical, 16)
         }
     }
     
-    private func isDateCompleted(_ date: Date) -> Bool {
-        habits.contains {habit in
-            habit.completedDates.contains {completedDate in
+    private func completionPercentage(for date: Date) -> Double {
+        guard !habits.isEmpty else { return 0.0 }
+
+        let completedCount = habits.filter { habit in
+            habit.completedDates.contains { completedDate in
                 Calendar.current.isDate(completedDate, inSameDayAs: date)
+
             }
-        }
+        }.count
+        
+        return Double(completedCount) / Double(habits.count)
     }
 }
 
