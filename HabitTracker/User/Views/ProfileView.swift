6659526cf8d482f14159037c638f5d68d0e38685
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-29 12:51:38 +0200

    Added ui info for guest user and connected login sheet to button

--- HabitTracker/User/Views/ProfileView.swift
+++ HabitTracker/User/Views/ProfileView.swift
@@ -9,43 +9,62 @@ import SwiftUI
 
 struct ProfileView: View {
     @Environment(AuthViewModel.self) private var authViewModel
+    @Environment(\.dismiss) private var dismiss
     
+    @State private var showLoginSheet: Bool = false
     var body: some View {
         
         NavigationStack {
-            
-            VStack() {
-                HStack {
-                    Spacer()
-                    NavigationLink {
-                        SettingsView()
-                            .environment(authViewModel)
-                    } label: {
-                        Image(systemName: "gear")
-                            .foregroundColor(.primaryText)
-                            .font(.system(size: 24))
-                            .padding(.trailing)
-            
-                            
-                    }
-                }
-                HStack {
-                    Image("profil")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 120, height: 120)
+            if authViewModel.authState == .guest {
+                VStack {
+                    Text("Du är inloggad som gäst")
+                        .font(.headline)
+                        .foregroundColor(.primaryText)
+                        .padding()
                     
-                   Spacer()
+                    Button {
+                        showLoginSheet = true
+                    } label: {
+                        Text("Skapa konto eller logga in")
+                    }
+                    .modifier(ButtonModifier())
+                }
+                .sheet(isPresented: $showLoginSheet) {
+                    LoginView()
+                }
+            } else {
+                VStack() {
+                    HStack {
+                        Spacer()
+                        NavigationLink {
+                            SettingsView()
+                                .environment(authViewModel)
+                        } label: {
+                            Image(systemName: "gear")
+                                .foregroundColor(.primaryText)
+                                .font(.system(size: 24))
+                                .padding(.trailing)
+                            
+                            
+                        }
+                    }
+                    HStack {
+                        Image("profil")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 120, height: 120)
+                        
+                        Spacer()
+                    }
+                    
+                    
+                    
+                    Spacer()
                 }
                 
-                
-                
-                Spacer()
             }
-          
+            
         }
-        
-        
     }
 }
 
