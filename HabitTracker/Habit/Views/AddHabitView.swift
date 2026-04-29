084401ad98645f5081700ad2c32d4d9f04277279
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-29 12:13:41 +0200

    created initial setup for addhabitview based on isguest or not

--- HabitTracker/Habit/Views/AddHabitView.swift
+++ HabitTracker/Habit/Views/AddHabitView.swift
@@ -8,7 +8,21 @@
 import SwiftUI
 
 struct AddHabitView: View {
+    
+    @Environment(HabitLocalViewModel.self) private var habitLocalViewModel
+    @Environment(HabitViewModel.self) private var habitViewModel
+    @Environment(AuthViewModel.self) private var authViewModel
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var habitName = ""
+    
+    var isGuest: Bool {
+        authViewModel.authState == .guest
+    }
+    
     var body: some View {
+        
+        
         NavigationStack {
             ZStack {
                 VStack {
@@ -20,17 +34,42 @@ struct AddHabitView: View {
                         Spacer()
                     }
                     .padding()
+               
+                    
+                    
+                    TextField("", text: $habitName,
+                        prompt: Text("Jag vill springa mer").foregroundColor(.gray))
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                        .modifier(TextFieldModifier())
+                        .foregroundStyle(.primaryText)
+                    
                     Spacer()
+                    
+                    Button {
+                        if isGuest {
+                            habitLocalViewModel.saveHabit(name: habitName)
+                        } else {
+                            Task {
+                                await habitViewModel.saveHabit(userId: authViewModel.currentUserId, habit: Habit(id: UUID().uuidString, name: habitName, completedDates: []))
+                            }
+                           
+                          
+                        }
+                        dismiss()
+                    } label: {
+                        Text("Spara")
+                    }
+                    .modifier(ButtonModifier())
+                    .disabled(habitName.isEmpty)
+                    .padding()
                 }
             }
             .gradientBackground()
         }
         .toolbar {
             ToolbarItem(placement: .title) {
-                Text("Lägg till vana")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.primaryText)
+                
             }
         }
       
