11b8adf0595713f28cb1ed7945d253f477e5b494
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-30 14:00:08 +0200

    created mapview that displays locations from habits

--- HabitTracker/Map/MapView.swift
+++ HabitTracker/Map/MapView.swift
@@ -6,10 +6,56 @@
 //
 
 import SwiftUI
-
+import MapKit
 struct MapView: View {
+    @Environment(HabitViewModel.self) private var habitViewModel
+    @Environment(HabitLocalViewModel.self) private var habitLocalViewModel
+    @Environment(AuthViewModel.self) private var authViewModel
+    
+
+    @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
+    
+    @State private var selectedLocation: Location? = nil
+    
+    private var locations: [Location] {
+           if authViewModel.authState == .guest {
+               return habitLocalViewModel.habits.flatMap { $0.locations }
+           } else {
+               return habitViewModel.habits.flatMap { $0.locations }
+           }
+       }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+   
+            Map(position: $position) {
+                UserAnnotation()
+                
+                ForEach(locations) {location in
+                    Annotation(location.name, coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)) {
+                        ZStack {
+                            Circle()
+                                .fill(LinearGradient(colors: [Color.theme.cardGradientStart, Color.theme.cardGradientEnd], startPoint: .topLeading, endPoint: .bottomTrailing))
+                                .frame(width: 38, height: 44)
+                            
+                            Image(systemName: "mappin")
+                                .foregroundStyle(.primaryText)
+                                .fontWeight(.bold)
+                        }
+                        .onTapGesture {
+                            selectedLocation = location
+                        }
+                        
+                    }
+                }
+            }
+            .mapControls {
+            MapUserLocationButton()
+            MapPitchToggle()
+        }
+        .sheet(item: $selectedLocation) { location in
+            LocationDetailSheet(location: location)
+        }
+        
+
     }
 }
 
