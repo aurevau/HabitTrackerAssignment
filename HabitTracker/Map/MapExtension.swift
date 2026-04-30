765e80ea37757b103d0e743081e5a94ca9721f4a
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-04-30 13:59:40 +0200

    added CLLocationupdate extension for async current location fetching

--- HabitTracker/Map/MapExtension.swift
+++ HabitTracker/Map/MapExtension.swift
@@ -5,4 +5,15 @@
 //  Created by Aurelie Vaudan APP25 on 2026-04-30.
 //
 
-import Foundation
+import CoreLocation
+
+extension CLLocationUpdate {
+    static func currentLocation() async throws -> CLLocation?{
+        for try await update in CLLocationUpdate.liveUpdates() {
+            if let location = update.location {
+                return location
+            }
+        }
+        return nil 
+    }
+}
