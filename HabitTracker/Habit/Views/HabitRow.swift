328a50998b06123f85ed7d28caaec5faf856f1d3
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-05-04 11:34:59 +0200

    changed background of habitrow if completed and sheet for opening camera and adding camera to habit when completed

--- HabitTracker/Habit/Views/HabitRow.swift
+++ HabitTracker/Habit/Views/HabitRow.swift
@@ -12,23 +12,30 @@ struct HabitRow: View {
     @Environment(HabitViewModel.self) private var habitViewModel
     @Environment(AuthViewModel.self) private var authViewModel
     
+    @State private var showingCamera = false
+    @State private var selectedImage: UIImage?
     
     let habit: Habit
     var body: some View {
+        
+        
         HStack {
             
             Button {
                 Task {
                     await habitViewModel.toggleToday(for: habit, userId: authViewModel.currentUserId)
+               
+                     if habit.isCompletedToday {
+                        showingCamera = false
+                    } else {
+                        showingCamera = true
+                    }
                 }
             } label: {
                 Image(systemName: habit.isCompletedToday ? "checkmark.circle.fill" : "circle")
                     .foregroundStyle(habit.isCompletedToday ? .green : .primaryText)
               
             }
-        
-          
-            
             Text(habit.name)
                 .fontWeight(.semibold)
                 .foregroundColor(.primaryText)
@@ -38,13 +45,27 @@ struct HabitRow: View {
             Text("🔥 \(habit.currentStreak)")
                 .font(.subheadline)
                 .foregroundColor(.primaryText)
+            
+            
+            
+            
         }
         .padding()
-        .gradientCard()
+        .gradientCard(isCompleted: habit.isCompletedToday)
         .listRowSeparator(.hidden)
         .listRowBackground(Color.clear)
         .listRowInsets(EdgeInsets(top: 4, leading: 16, bottom: 4, trailing: 16))
         .animation(.spring(response: 0.3), value: habit.isCompletedToday)
+        .sheet(isPresented: $showingCamera) {
+            CameraView(image: $selectedImage)
+        }
+        .onChange(of: selectedImage) {_, newImage in
+            if let image = newImage {
+                Task {
+                    await habitViewModel.addImageToHabit(userId: authViewModel.currentUserId, habit: habit, image: image)
+                }
+            }
+        }
         
     }
 }
