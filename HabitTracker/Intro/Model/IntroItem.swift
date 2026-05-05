2d97e3141bf19bee33be0af3a207cb77b061e498
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-05-05 14:47:47 +0200

    added firstTimeUser to authState to be able to handle navigation between screens better

---
+++ HabitTracker/Intro/Model/IntroItem.swift
@@ -0,0 +1,8 @@
+//
+//  IntroItem.swift
+//  HabitTracker
+//
+//  Created by Aurelie Vaudan APP25 on 2026-05-05.
+//
+
+import Foundation
---
+++ HabitTracker/Intro/Views/IntroView.swift
@@ -0,0 +1,18 @@
+//
+//  IntroView.swift
+//  HabitTracker
+//
+//  Created by Aurelie Vaudan APP25 on 2026-05-05.
+//
+
+import SwiftUI
+
+struct IntroView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroView()
+}
