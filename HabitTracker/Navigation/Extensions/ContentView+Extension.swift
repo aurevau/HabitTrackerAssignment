c6c566a02467550dae097fa6b3382a4b266deea2
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-05-06 14:08:24 +0200

    restructured extension into correct folder

---
+++ HabitTracker/Navigation/Extensions/ContentView+Extension.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView+Extension.swift
+//  HabitTracker
+//
+//  Created by Aurelie Vaudan APP25 on 2026-05-05.
+//
+
+import Foundation
+
+extension ContentView {
+    
+    func migrateHabitsToFirebase(habitLocalViewModel: HabitLocalViewModel, userId: String, habitViewModel: HabitViewModel) async {
+        let localHabits = habitLocalViewModel.habits
+        
+        guard !localHabits.isEmpty else {
+            return
+        }
+        
+        await habitViewModel.migrateLocalHabits(localHabits: localHabits, userId: userId)
+        
+        if habitViewModel.errorMessage == nil {
+            for habit in localHabits {
+                habitLocalViewModel.deleteHabit(habit)
+            }
+        } else {
+            //Migration Failed
+        }
+    }
+}
